1635e009646cf442e43fc448a631010cd32f0497
ngianguyen81 <ngianguyen81@gmail.com>
2022-03-01 16:36:38 +0700

    add gameManager

---
+++ Assets/Game/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool gameOver = true;
+    //public GameObject player;
+
+    private UIManager _uIManager;
+
+    private void Start()
+    {
+        _uIManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+    }
+
+    private void Update()
+    {
+        if (gameOver)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //Instantiate(player, new Vector3(0, -4.2f, 0), Quaternion.identity);
+                gameOver = false;
+                _uIManager.HideTitleScreen();
+            }
+        }
+    }
+}
