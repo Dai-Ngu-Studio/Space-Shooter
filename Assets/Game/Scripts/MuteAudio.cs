7495bf35dbb64be45ebccbff3d821f3fa959da9a
Nguyễn Dũng <dpron12345@gmail.com>
2022-03-01 23:52:20 +0700

    add mute sound

---
+++ Assets/Game/Scripts/MuteAudio.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MuteAudio : MonoBehaviour
+{
+    public void MuteToggle(bool muted)
+    {
+        if (muted)
+        {
+            AudioListener.volume = 0;
+        }
+        else
+        {
+            AudioListener.volume = 1;
+        }
+    }
+}
