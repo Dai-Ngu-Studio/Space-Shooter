a09903efe4b1ecbf38b593ce75feb4bda936eff7
ngianguyen81 <ngianguyen81@gmail.com>
2022-03-01 16:31:59 +0700

    add script for lives, score

---
+++ Assets/Game/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Sprite[] lives;
+    public Image livesImageDisplay;
+    public Text scoreText;
+    public int score;
+    public GameObject titleScreen;
+
+    public void UpdateLives(int currentLive)
+    {
+        Debug.Log(currentLive);
+        livesImageDisplay.sprite = lives[currentLive];
+    }
+
+    public void UpdateScore()
+    {
+        score += 10;
+
+        scoreText.text = "Score: " + score;
+    }
+
+    public void ShowTitleScreen()
+    {
+        titleScreen.SetActive(true);
+    }
+
+    public void HideTitleScreen()
+    {
+        titleScreen.SetActive(false);
+        scoreText.text = "Scrore:";
+    }
+}
