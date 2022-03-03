642441bc54f0d84ed1a805469d674eed43f42b8a
HT-Dat <hotiendat661@gmail.com>
2022-03-03 17:44:30 +0700

    Add shooting laser

---
+++ Assets/Game/Scripts/Laser.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float _speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        if (transform.position.y >= 6.0)
+        {
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    private float speed = 5.0f;
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private GameObject _laserPrefab;
+    [SerializeField] private float _fireRate = 0.25f;
+
+    private float _canFire = 0.0f; // ?? :) ??
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +20,10 @@ public class Player : MonoBehaviour
     void Update()
     {
         Movement();
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+            Shoot();
+        }
     }
 
     private void Movement()
@@ -45,4 +52,14 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(-8f, transform.position.y, 0);
         }
     }
+
+    private void Shoot()
+    {
+        if (Time.time > _canFire)
+        {
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity); //:pain:
+            _canFire = Time.time + _fireRate;
+        }
+    }
 }
+/**/
